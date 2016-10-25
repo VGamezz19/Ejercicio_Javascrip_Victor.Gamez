bebc82528203c78aeec5d1f1c5af7a87a81834e2
Victor.Gamez <pictor.games30@gmail.com>
2016-10-25 16:16:36 +0200

    Practica_Horario_JavaScrip_HTML_CSS

---
+++ Practica_Hrario/js/app.js
@@ -0,0 +1,76 @@
+
+
+function offtablas() {
+
+    	tabla1.style.display='none';
+    	tabla2.style.display='none';
+   		tabla3.style.display='none';
+   		footer.style.display='none';
+
+}
+     
+function ontablas() {
+
+	if (desplegable.options[0].selected) {
+		body.style.background= 'rgba(55, 187, 186, 0.39)';
+		tabla1.style.display="none";
+		tabla2.style.display="none";
+		tabla3.style.display='none';
+		footer.style.display='none';
+		     
+	} 
+
+	if (desplegable.options[1].selected) {
+		body.style.background= 'rgba(55, 187, 186, 0.39)';
+		tabla1.style.display="inline-table";
+		footer.style.display='inline';
+		tabla2.style.display="none";
+		tabla3.style.display='none';   	
+	} 
+
+	if (desplegable.options[2].selected) {
+		body.style.background= 'rgba(55, 187, 186, 0.39)';
+		tabla2.style.display="inline-table";
+		tabla1.style.display="inline-table";
+		tabla1.style.display="none";
+		tabla3.style.display='none';	     
+		     	
+	}	  
+
+	if (desplegable.options[3].selected) {
+		body.style.background= 'rgba(55, 187, 186, 0.39)';
+		tabla3.style.display='inline-table';
+		tabla1.style.display="inline-table";
+		tabla1.style.display="none";
+		tabla2.style.display="none";
+		 	
+	}
+}
+
+
+
+
+
+ /*function presionar() {
+
+	 	body.style.background='#fff';
+  		tabla1.style.display='none';
+ 		tabla2.style.display='none';
+  		tabla3.style.display='none';
+
+}
+
+function levantar() {
+   var largeImage = document.getElementById('tabla1');
+   body.style.background= 'rgba(55, 187, 186, 0.39)';
+   largeImage.style.display = 'block';
+   largeImage.style.width=200+"px";
+   largeImage.style.height=200+"px";
+   var url=largeImage.getAttribute('tabla1');
+   window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
+
+} */ 
+
+
+
+
